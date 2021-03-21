afcfcf9be6dc5d627f29056a0187bad1a7737cf4
RopeTeacher <orochiandcris@163.com>
2021-03-21 19:11:03 +0800

    1

---
+++ ajax/day01/server.js
@@ -0,0 +1,46 @@
+//引入express
+const express = require('express');
+
+//创建应用对象
+const app = express();
+
+//创建路由规则
+app.get('/server',(request,response)=>{
+    response.setHeader("Access-Control-Allow-Origin", "*")
+    //设置响应
+    response.send('HELLO EXPRESS');
+});
+
+app.post('/server',(request,response)=>{
+    response.setHeader("Access-Control-Allow-Origin","*")
+    response.send('success - 2')
+});
+
+app.all('/json-server',(request,response)=>{
+    response.setHeader("Access-Control-Allow-Origin","*")
+    response.setHeader("Access-Control-Allow-Header","*")
+    const data = {
+        id: 1,
+        name: "jack",
+        age: 7
+    }
+    let s = JSON.stringify(data);
+    response.send(s)
+});
+
+app.get('/ie',(request,response)=>{
+    response.setHeader("Access-Control-Allow-Origin","*")
+    response.send('ie缓存')
+});
+
+app.get('/delay',(request,response)=>{
+    response.setHeader("Access-Control-Allow-Origin","*")
+    setTimeout(()=>{
+        response.send('延时发送')
+    },3000);
+});
+
+//监听端口 启动服务
+app.listen(8000,()=>{
+    console.log("服务已启动 8000端口监听中")
+})
